da8d0fe428e986d423fcc726ebbfd1fd374be14b
Ismael Garrido Mansoa <isma@wifi-216-246.uc3m.es>
2018-03-08 20:34:38 +0100

    venga va

--- client.c
+++ client.c
@@ -9,23 +9,24 @@ int main(void) {
 
   //Initialization of the system
   printf("Initializing  the system\n");
+  float fnumber = 5.0;
   init();
-
+  
   //Insertion of an element
   printf("Inserting the triplet [5, esketit, 5.0]\n");
-  set_value(5, "esketit", 5.0);
+  set_value(5, "esketit", fnumber);
 
   //Obtain the value associated to a key
   printf("Obtain the value associated to key -> 5\n");
-  get_value(5, "esketit", 5.0);
-  
+  get_value(5, "esketit", &fnumber);
+
   //Modify the associated values to the given ky by the ones specied in the method
   printf("Look for key: '%d' and change its associated values by value1 = %d and value 2 = %f\n", req.key, req.value1, req.value2);
-  modify_value(5, "esketit", 5.0);
+  modify_value(5, "esketit", &fnumber);
 
   //Delete the elements with the given key
-  printf("Delete the element who's key is: %d\n", key);
-  delete(5);
+  printf("Delete the element who's key is: 5\n");
+  delete_key(5);
 
   //Return the number of items stored on the server
   int items = num_items();
