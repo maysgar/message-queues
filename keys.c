5217e14b9d56f402d38c2b6122d57a0904bd6d55
Ismael Garrido Mansoa <isma@wifi-216-246.uc3m.es>
2018-03-08 18:44:30 +0100

    sdfsdfds

--- keys.c
+++ keys.c
@@ -43,11 +43,11 @@ int set_value(int key, char *value1, float value2){
 }
 
 int get_value(int key, char *value1, float *value2){
-  return send(3, key, value1, value2);
+  return send(3, key, value1, &value2);
 }
 
 int modify_value(int key, char *value1, float *value2){
-  return send(4, key, value1, value2);
+  return send(4, key, value1, &value2);
 }
 
 int delete_key(int key){
