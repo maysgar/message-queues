a766dabd42db115681645dd35172e7fdccc67a3a
Ismael Garrido Mansoa <isma@wifi-216-246.uc3m.es>
2018-03-08 19:58:27 +0100

    añadidos definiciones a keys.h

--- keys.h
+++ keys.h
@@ -8,3 +8,15 @@ struct triplet{
  char client_queue_name [MAXSIZE];
  int method_id; //identifier to know what method the client requests
 }
+
+int init();
+
+int set_value(int key, char *value1, float value2);
+
+int get_value(int key, char *value1, float *value2);
+
+int modify_value(int key, char *value1, float *value2);
+
+int delete_key(int key);
+
+int num_items();
